86e76c9267760a82f8bbf05994cdcb05abebf7c5
surajspider <ramsuraj14@gmail.com>
2023-12-01 15:51:44 +0530

    cat hover

--- src/Compo/CategoriesPage.js
+++ src/Compo/CategoriesPage.js
@@ -1,7 +1,9 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { storeData } from '../DataStore/DataStore'
 import { useParams } from 'react-router-dom';
 import RatingStars from './RatingStars';
+import { FaStar } from 'react-icons/fa';
+import Footer from './Footer';
 
 function CategoriesPage() {
     const [datas] = useContext(storeData);
@@ -12,6 +14,40 @@ function CategoriesPage() {
     const name = useParams().name;
     console.log(name);
     let subcats = [];
+    const instructors = [
+        {
+            name: "Dr. Angela Yu",
+            course: "Python, Datascience",
+            rating: 4.6,
+            students: 7894561,
+            courses: 7,
+            img: "https://img-c.udemycdn.com/user/75x75/31334738_a13c_3.jpg"
+        },
+        {
+            name: "Maximillian",
+            course: "React JS, React Hooks",
+            rating: 4.3,
+            students: 794561,
+            courses: 42,
+            img: "https://img-c.udemycdn.com/user/75x75/31926668_94e7_6.jpg"
+        },
+        {
+            name: "Jonas",
+            course: "Javascript, React JS",
+            rating: 4.8,
+            students: 784561,
+            courses: 48,
+            img: "https://img-c.udemycdn.com/user/75x75/7799204_2091_5.jpg"
+        },
+        {
+            name: "Jose Portilla",
+            course: "Python, Datascience",
+            rating: 4.0,
+            students: 94561,
+            courses: 54,
+            img: "https://img-c.udemycdn.com/user/75x75/9685726_67e7_4.jpg"
+        }
+    ]
 
     if (name === "development") {
         subcats = devcatname;
@@ -21,6 +57,15 @@ function CategoriesPage() {
     console.log(subcats);
     const filtereddatas = datas.filter(item => item.category === name);
     console.log(filtereddatas);
+    const [isHovered, setIsHovered] = useState(null);
+
+    const handleContainerHover = (index) => {
+        setIsHovered(index);
+    };
+
+    const handleContainerLeave = () => {
+        setIsHovered(null);
+    };
     return (
         <div>
             <div className='navsub'>
@@ -45,7 +90,8 @@ function CategoriesPage() {
                     {filtereddatas.filter((item) => item.id % 2 === 0).slice(0, 5).map((item, index) => {
                         const bestsel = index === 1 || index === 3;
                         return (
-                            <div className='mostitem_div' key={index}>
+                            <div className={`mostitem_div ${isHovered === index ? 'hovered' : ''}`} key={index} onMouseEnter={() => handleContainerHover(index)} onMouseLeave={handleContainerLeave}>
+
                                 <div className='mostpop_img'>
                                     <img src={item.image} alt='not found' className='imgfil' />
                                 </div>
@@ -56,6 +102,26 @@ function CategoriesPage() {
                                     <h4 style={{ marginTop: "1%" }}>₹{item.offerPrice}</h4>
                                     {bestsel && <h4 className='bestsel'>Bestseller</h4>}
                                 </div>
+                                {isHovered === index && (
+                                    <div className='arrow-container'>
+                                        <div className="arrow"></div>
+                                        <div className='additional-content'>
+                                            <h4 style={{ margin: 0 }}>{item.courseName}</h4>
+                                            <h5>What you'll learn</h5>
+                                            <ul className='pointlist'>
+                                                <li>{item.point1}</li>
+                                                <li>{item.point2}</li>
+                                                <li>{item.point3}</li>
+                                            </ul>
+                                            <div className='buttondiv'>
+                                                <button className='addtocart'>Add to Cart</button>
+                                                <div className='heartbutton_img'>
+                                                    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSf9nKX4mmWawwlgehpJGHNxT5OcxKVlRsIzA&usqp=CAU' alt='not found' className='imgfil' />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                )}
                             </div>
                         )
                     })}
@@ -72,8 +138,61 @@ function CategoriesPage() {
                 </div>
                 <div className='popinstruc_div'>
                     <h2>Popular Instructors</h2>
+                    <div className='popinstruc_maindiv'>
+                        {instructors.map((item, index) => {
+                            return (
+                                <div className='popinstruc_main' key={index}>
+                                    <div className='imginstruc_div'>
+                                        <img src={item.img} alt='not found' className='immgfil' />
+                                    </div>
+                                    <div className='instruc_content'>
+                                        <h4 style={{ marginBottom: 0, marginTop: "3%" }}>{item.name}</h4>
+                                        <p style={{ margin: 0 }}>{item.course}</p>
+                                        <p style={{ margin: 0 }}>{item.rating}<FaStar color='#ffb800' />Instructor Rating</p>
+                                        <p style={{ margin: 0 }}>{item.students} Students</p>
+                                        <p style={{ margin: 0 }}>{item.courses} courses</p>
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
+                <div className='allcourses_div'>
+                    <h2>All {name} Courses</h2>
+                    <div className='allcourses_maindiv'>
+                        <div className='leftcontainer'>
+                            <h4>Rating</h4>
+                        </div>
+                        <div className='rightcontainer'>
+
+                            {filtereddatas.slice(0, 8).map((item, index) => {
+                                return (
+                                    <div key={index}>
+                                        <div className='item_maindiv'>
+                                            <div className='item_imgdiv'>
+                                                <img src={item.image} alt='not found' className='imgfil' />
+                                            </div>
+                                            <div className='item_contentdiv'>
+                                                <h4 className='padd'>{item.courseName}</h4>
+                                                <p className='padd'>{item.creator}</p>
+                                                <div className='rating_div'>{item.rating}<div><RatingStars rating={item.rating} /></div>(3256)</div>
+                                                <p className='padd'>{item.hrs} total hours . {item.lectures} lectures . All levels</p>
+                                            </div>
+                                            <div className='item_pricediv'>
+                                                <h4>₹{item.offerPrice}</h4>
+                                            </div>
+                                        </div>
+                                        <hr />
+                                    </div>
+                                )
+                            })}
+
+
+                        </div>
+                    </div>
                 </div>
             </div>
+            <Footer />
         </div>
     )
 }
