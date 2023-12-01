372f9bf8b38d075a7b672276343b3ce780f94f74
surajspider <ramsuraj14@gmail.com>
2023-12-01 17:56:08 +0530

    category completed

--- src/Compo/CategoriesPage.js
+++ src/Compo/CategoriesPage.js
@@ -3,9 +3,12 @@ import { storeData } from '../DataStore/DataStore'
 import { useParams } from 'react-router-dom';
 import RatingStars from './RatingStars';
 import { FaStar } from 'react-icons/fa';
+import { IoMdArrowDropdown } from "react-icons/io";
 import Footer from './Footer';
 
 function CategoriesPage() {
+    const [starview, setstarview] = useState(true);
+    const [videoview, setvideo] = useState(true);
     const [datas] = useContext(storeData);
     console.log(datas);
     const devcatname = ["web development", "data science", "mobile development", "programming language"];
@@ -161,7 +164,95 @@ function CategoriesPage() {
                     <h2>All {name} Courses</h2>
                     <div className='allcourses_maindiv'>
                         <div className='leftcontainer'>
-                            <h4>Rating</h4>
+                            <div className="ratingdropdown" onClick={() => setstarview(!starview)}>
+                                <h2>Rating</h2>
+                                <div className={`droplogo_div ${!starview ? 'rotate' : 'rotateflip'}`}><IoMdArrowDropdown size={"2em"} /></div>
+                            </div>
+                            {starview && (
+                                <div>
+                                    <div className='stars_div'>
+                                        <input name='star' type='radio'></input>
+                                        <RatingStars rating={4.5} />
+                                        <p style={{ margin: 0 }}>&nbsp;4.5 & up (10,000)</p>
+                                    </div>
+                                    <div className='stars_div'>
+                                        <input name='star' type='radio'></input>
+                                        <RatingStars rating={4} />
+                                        <p style={{ margin: 0 }}>&nbsp;4 & up (10,000)</p>
+                                    </div>
+                                    <div className='stars_div'>
+                                        <input name='star' type='radio'></input>
+                                        <RatingStars rating={3.5} />
+                                        <p style={{ margin: 0 }}>&nbsp;3.5 & up (10,000)</p>
+                                    </div>
+                                    <div className='stars_div'>
+                                        <input name='star' type='radio'></input>
+                                        <RatingStars rating={3} />
+                                        <p style={{ margin: 0 }}>&nbsp;3 & up (10,000)</p>
+                                    </div>
+                                </div>
+                            )}
+                            <hr />
+                            <div className="ratingdropdown" onClick={() => setvideo(!videoview)}>
+                                <h3>Video Duration</h3>
+                                <div className={`droplogo_div ${!videoview ? 'rotate' : 'rotateflip'}`}><IoMdArrowDropdown size={"2em"} /></div>
+                            </div>
+                            {videoview && (
+                                <div>
+                                    <div className='stars_div'>
+                                        <input name='star' type='checkbox'></input>
+                                        <p style={{ margin: 0 }}>&nbsp;0 - 1 hr (667)</p>
+                                    </div>
+                                    <div className='stars_div'>
+                                        <input name='star' type='checkbox'></input>
+                                        <p style={{ margin: 0 }}>&nbsp;1 - 3 hr (667)</p>
+                                    </div>
+                                    <div className='stars_div'>
+                                        <input name='star' type='checkbox'></input>
+                                        <p style={{ margin: 0 }}>&nbsp;3 - 6 hr (667)</p>
+                                    </div>
+                                    <div className='stars_div'>
+                                        <input name='star' type='checkbox'></input>
+                                        <p style={{ margin: 0 }}>&nbsp;6 - 17 hr (667)</p>
+                                    </div>
+                                </div>
+                            )}
+                            <hr />
+                            <div className="ratingdropdown">
+                                <h3>Topic</h3>
+                                <div className={`droplogo_div`}><IoMdArrowDropdown size={"2em"} /></div>
+                            </div>
+                            <hr />
+                            <div className="ratingdropdown">
+                                <h3>Subcategory</h3>
+                                <div className={`droplogo_div`}><IoMdArrowDropdown size={"2em"} /></div>
+                            </div>
+                            <hr />
+                            <div className="ratingdropdown">
+                                <h3>Level</h3>
+                                <div className={`droplogo_div`}><IoMdArrowDropdown size={"2em"} /></div>
+                            </div>
+                            <hr />
+                            <div className="ratingdropdown">
+                                <h3>Language</h3>
+                                <div className={`droplogo_div`}><IoMdArrowDropdown size={"2em"} /></div>
+                            </div>
+                            <hr />
+                            <div className="ratingdropdown">
+                                <h3>Price</h3>
+                                <div className={`droplogo_div`}><IoMdArrowDropdown size={"2em"} /></div>
+                            </div>
+                            <hr />
+                            <div className="ratingdropdown">
+                                <h3>Features</h3>
+                                <div className={`droplogo_div`}><IoMdArrowDropdown size={"2em"} /></div>
+                            </div>
+                            <hr />
+                            <div className="ratingdropdown">
+                                <h3>Subtitles</h3>
+                                <div className={`droplogo_div`}><IoMdArrowDropdown size={"2em"} /></div>
+                            </div>
+                            <hr />
                         </div>
                         <div className='rightcontainer'>
 
@@ -186,8 +277,6 @@ function CategoriesPage() {
                                     </div>
                                 )
                             })}
-
-
                         </div>
                     </div>
                 </div>
